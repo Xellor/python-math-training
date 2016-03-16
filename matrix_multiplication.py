b7a322242a702f4e7f961ddf8faff188bdb8709d
Xellor <xellor.me@gmail.com>
2016-03-17 00:48:06 +0300

    Matrix (multiplication & summation/difference) complete.

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,17 +1,8 @@
 # Matrix (multiplication)
 # Python3
-# Last update 27.02.2016
-print('Matrix: multiplication\n\n')
-try:
-	matrix_n = int(input('Enter matrix size (N x N): '))	# Set size matrix
-except ValueError:
-	print('Wrong value, only integer!')
-	input('(pause)')
-	exit()
-if matrix_n < 1:	# Check
-	print('Error, matrix size < 0')
-	input('(pause)')
-	exit()
+# by Xellor
+# Last update 16.03.2016
+matrix_n = int(input('Enter matrix size (N x N): '))	# Set size matrix, integer only
 matrix_1 = []	# Initialize matrix variables
 matrix_2 = []
 matrix_3 = []	# matrix_3 is result matrix
@@ -21,30 +12,12 @@ for i in range(matrix_n):	# Append in matrix variables N lists
 for i in range(matrix_n):
 	for j in range(matrix_n):	# Input N values in matrix_1 lists
 		print('Enter M1(' + str(i + 1) + '.' + str(j + 1) + ')')
-		try:
-			matrix_1[i].append(int(input()))
-		except ValueError:
-			print('Only integer values!')
-			input('(pause)')
-			exit()
+		matrix_1[i].append(int(input()))
 print('Now M2')
 for i in range(matrix_n):
 	for j in range(matrix_n):	# Input N values in matrix_2 lists
 		print('Enter M2(' + str(i + 1) + '.' + str(j + 1) + ')')
-		try:
-			matrix_2[i].append(int(input()))
-		except ValueError:
-			print('Only integer values!')
-			input('(pause)')
-			exit()
-print('\n\n\nYou matrix:\n')
-for i in range(matrix_n):
-	print(matrix_1[i])
-print('\n')
-for i in range(matrix_n):
-	print(matrix_2[i])
-print('\n')
-print('Multiplicating...')
+		matrix_2[i].append(int(input()))
 s = 0	# temp sum
 t = []	# temp matrix
 for z in range(matrix_n):
@@ -55,14 +28,7 @@ for z in range(matrix_n):
 		s = 0
 	matrix_3.append(t)
 	t = []
-print('\n\nM1:')
-for i in range(matrix_n):
-	print(matrix_1[i])
-print('\nM2:')
-for i in range(matrix_n):
-	print(matrix_2[i])
 print('\nM3 (result):')
 for i in range(matrix_n):
 	print(matrix_3[i])
-input('\n\n(pause)')
 exit()
--- matrix_summation_and_difference.py
+++ matrix_summation_and_difference.py
@@ -1,17 +1,8 @@
 # Matrix (summation and difference)
 # Python3
-# Last update 15.02.2016
-print('Matrix: summation and difference\n\n')
-try:
-	matrix_n = int(input('Enter matrix size (N x N): '))	# Set size matrix
-except ValueError:
-	print('Wrong value, only integer!')
-	input('(pause)')
-	exit()
-if matrix_n < 1:	# Check
-	print('Error, matrix size < 0')
-	input('(pause)')
-	exit()
+# by Xellor
+# Last update 16.03.2016
+matrix_n = int(input('Enter matrix size (N x N): '))	# Set size matrix, integer only
 matrix_1 = []	# Initialize matrix variables
 matrix_2 = []
 matrix_3 = []	# matrix_3 is result matrix
@@ -22,36 +13,15 @@ for i in range(matrix_n):	# Append in matrix variables N lists
 for i in range(matrix_n):
 	for j in range(matrix_n):	# Input N values in matrix_1 lists
 		print('Enter M1(' + str(i + 1) + '.' + str(j + 1) + ')')
-		try:
-			matrix_1[i].append(int(input()))
-		except ValueError:
-			print('Only integer values!')
-			input('(pause)')
-			exit()
+		matrix_1[i].append(int(input()))
 print('Now M2')
 for i in range(matrix_n):
 	for j in range(matrix_n):	# Input N values in matrix_2 lists
 		print('Enter M2(' + str(i + 1) + '.' + str(j + 1) + ')')
-		try:
-			matrix_2[i].append(int(input()))
-		except ValueError:
-			print('Only integer values!')
-			input('(pause)')
-			exit()
-print('\n\n\nYou matrix:\n')
-for i in range(matrix_n):
-	print(matrix_1[i])
+		matrix_2[i].append(int(input()))
 print('\n')
-for i in range(matrix_n):
-	print(matrix_2[i])
-print('\n')
-print('What you want to do with them?')
-print('1 - plus\n2 - minus')
+print('1 - plus; 2 - minus;')
 matrix_action = int(input('Enter action No: '))
-if matrix_action != 1 and matrix_action != 2:	# Check
-	print('Wrong action No!')
-	input('(pause)')
-	exit()
 if matrix_action == 1:
 	for i in range(matrix_n):
 		for j in range(matrix_n):
@@ -60,14 +30,7 @@ elif matrix_action == 2:
 	for i in range(matrix_n):
 		for j in range(matrix_n):
 			matrix_3[i].append(matrix_1[i][j] - matrix_2[i][j])
-print('\n\nM1:')
-for i in range(matrix_n):
-	print(matrix_1[i])
-print('\nM2:')
-for i in range(matrix_n):
-	print(matrix_2[i])
 print('\nM3 (result):')
 for i in range(matrix_n):
 	print(matrix_3[i])
-input('\n\n(pause)')
 exit()
