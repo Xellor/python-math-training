affdb134c3d980a028e958c934532007b64a624a
Xellor <xellor.me@gmail.com>
2016-03-16 19:09:22 +0300

    Interpolations (linear & lagranhe methods) compleate.

--- interpolation_lagranje.py
+++ interpolation_lagranje.py
@@ -1,35 +1,19 @@
-# Interpolation (lagranje method)
+# Interpolation (Lagranje method)
 # Python3
-# Last update 15.02.2016
-print('Interpolation (lagranje)\n\n')
-x_values = input('Enter Xi values (1,2,3,..,n): ')   # input Xi values
-x_values = x_values.split(',')
+# by Xellor
+# Last update 16.03.2016
+x_values = input('Enter Xi values (1,2,3,..,n): ')   # input Xi values,
+x_values = x_values.split(',')						 # number of values Xi == Yi
 x_values_len = len(x_values)
-print(str(x_values_len) + ' positions')
 y_values = input('Enter Yi values (1,2,3,..,n): ')   # inpur Yi values
 y_values = y_values.split(',')
 y_values_len = len(y_values)
-print(str(y_values_len) + ' positions\n\n')
-if x_values_len != y_values_len:  # lenght check <len(Xi == Yi)>
-    print('Error, different length of Xi and Yi')
-    print('Xi = ' + str(x_values_len) + '\nYi = ' + str(y_values_len))
-    input('(pause)')
-    exit()
-try:
-    xt = float(input('Enter Xi to find: ')) # input xt (x to find)
-    print('\n\n')
-except ValueError:
-    print('Error, wrong input value!')
-    input('(pause)')
-    exit()
-# Polenom Lagranja
-polinom_up = ''
-polinom_down = ''
-p_up = float(1)
-p_down = float(1)
+xt = float(input('Enter Xi to find: ')) # input xt (x to find)
+p_up = float(1)		# numerator of polynom
+p_down = float(1)	# denominator of polynom
 p = float(0)
 for i in range(x_values_len):
-	p_up = 1
+	p_up = 1	# reset data
 	p_down = 1
 	for j in range(x_values_len):
 		x_index = j
@@ -37,14 +21,6 @@ for i in range(x_values_len):
 			continue
 		p_up = p_up * (xt - float(x_values[x_index]))
 		p_down = p_down * (float(x_values[i]) - float(x_values[x_index]))
-		polinom_up = polinom_up + '(' + str(xt) + ' - ' + str(float(x_values[x_index])) + ')'
-		polinom_down = polinom_down + '(' + str(x_values[i]) + '-' + str(x_values[x_index]) + ')'
 	p = p + float(y_values[i]) * (p_up / p_down)
-	print('   ' + str(polinom_up))
-	print(str(y_values[i]) + ' * -----------')
-	print('   ' + str(polinom_down))
 	print('\n')
-	polinom_up = ''
-	polinom_down = ''
 print(p)
-input()
--- interpolation_linear.py
+++ interpolation_linear.py
@@ -1,62 +1,30 @@
 # Interpolation (linear method)
 # Python3
-# Last update 13.02.2016
-print('Interpolation (linear)\n\n')
-x_values = input('Enter Xi values (1,2,3,..,n): ')   # input Xi values
-x_values = x_values.split(',')
+# by Xellor
+# Last update 16.03.2016
+x_values = input('Enter Xi values (1,2,3,..,n): ')   # input Xi values,
+x_values = x_values.split(',')                       # number of values Xi == Yi
 x_values_len = len(x_values)
-print(str(x_values_len) + ' positions')
 y_values = input('Enter Yi values (1,2,3,..,n): ')   # inpur Yi values
 y_values = y_values.split(',')
 y_values_len = len(y_values)
-print(str(y_values_len) + ' positions')
-if x_values_len != y_values_len:  # lenght check <len(Xi == Yi)>
-    print('Error, different length of Xi and Yi')
-    print('Xi = ' + str(x_values_len) + '\nYi = ' + str(y_values_len))
-    input('(pause)')
-    exit()
-try:
-    xt = float(input('Enter Xi to find: ')) # input xt (x to find)
-except ValueError:
-    print('Error, wrong input value!')
-    input('(pause)')
-    exit()
+xt = float(input('Enter Xt (Xi to find): ')) # input xt (x to find)
 x1 = float(xt)
 x2 = float(xt)
 for i in range(x_values_len):     # search x1, y1
-    try:
-        if float(x_values[i]) < x1:
-            continue
-        else:
-            x1 = float(x_values[i - 1])
-            y1 = float(y_values[i - 1])
-            break
-    except ValueError:
-        print('Error, wrong input value!')
-        input('(pause)')
-        exit()
+    if float(x_values[i]) < x1:
+        continue
+    else:
+        x1 = float(x_values[i - 1])
+        y1 = float(y_values[i - 1])
+        break
 for i in range(x_values_len):     # search x2, y2
     if float(x_values[i]) < x2:
         continue
     else:
-        try:
-            x2 = float(x_values[i])
-            y2 = float(y_values[i])
-        except ValueError:
-            print('Error, wrong input value!')
-            input('(pause)')
-            exit()
+        x2 = float(x_values[i])
+        y2 = float(y_values[i])
         break
-try:
-    y_find = y1 + ((y2 - y1) / (x2 - x1)) * (xt - x1)
-except NameError:
-    print('Wrong value in Xi (to find)!')
-    input('(pause)')
-    exit()
-except ZeroDivisionError:
-    print('Wrong syntasys')
-    input('(pause)')
-    exit()
-print('Yi founded: ' + str(y_find)) # output result
-input('(pause)')
+y_find = y1 + ((y2 - y1) / (x2 - x1)) * (xt - x1)   # calculation of Xt
+print('Yi founded: ' + str(y_find))
 exit()
